81e7b912686251644503c52b67e599013aa8e9bc
TOGoS <togos00@gmail.com>
2025-12-20 00:14:23 -0600

    p2285: round cap

--- make.ts
+++ make.ts
@@ -1071,7 +1071,7 @@ const builder = new Builder({
 		}),
 		...multiOsdBuildRules("2023/experimental/Threads2.scad", [
 			// Maybe good camera settings for hex heads
-			"p2214","p2215",
+			"p2214","p2215","p2285",
 		], {
 			openScadCmd: OPENSCAD2024_MANIFOLD_CMD,
 			cameraPosition: [-60, -60, 140],
