1f99558a8098f2fe825e63fa777550757950f0b4
TOGoS <togos00@gmail.com>
2026-01-04 11:44:02 -0600

    p2326: 3"x1" bucket handle grip

--- make.ts
+++ make.ts
@@ -1054,7 +1054,7 @@ const builder = new Builder({
 		}),
 		...multiOsdBuildRules("2023/experimental/Threads2.scad", [
 			"p2212", "p2220", "p2234", "p2311", "p2312", "p2314", "p2315", "p2318","p2319",
-			"p2320",
+			"p2320", "p2326",
 		], {
 			openScadCmd: OPENSCAD2024_MANIFOLD_CMD,
 			cameraPosition: [ 60, 120, 140],
